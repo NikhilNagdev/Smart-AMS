2eb04a8afd032340b8b18a4288cd84b4fa555d80
Nikhil Nagdev <nikhil.nagdev@outlook.com>
2020-01-18 03:46:56 +0530

    Added ajax controller

---
+++ app/Http/Controllers/AjaxController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AjaxController extends Controller
+{
+    public function location(Request $request) {
+        $lat = 19.1927;
+        $longt = 73.1778;
+        if(($request->lat+0.0010)>=$lat && ($request->lat-0.0010)<=$lat)
+        {
+            if(($request->longt+0.0010)>=$longt && ($request->longt-0.0010)<=$longt)
+            {
+                $msg = "Sucessfull";
+            }   else
+            {
+                $msg = "Wrong location";
+            }
+        }else{
+            echo "Abhi b wrong location";
+        }
+        return response()->json(array('msg'=> $msg), 200);
+    }
+}
